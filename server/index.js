39c27640bc18873e6436b1cc823ed1408fc9678d
bloom510 <bloomjazzer@gmail.com>
2018-08-25 11:34:51 -0700

    Mongo URI

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const cors = require('cors');
 //DB setup
-mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true })
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:auth/auth', { useNewUrlParser: true })
 
 //App setup
 app.use(morgan('combined'));
