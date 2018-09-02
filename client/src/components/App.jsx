602371f292dccded99b0dc93ba3cfd6a80ad02d1
bloom510 <bloomjazzer@gmail.com>
2018-09-01 18:23:07 -0700

    Removes grid

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from './Header';
-import Grid from '@material-ui/core/Grid';
-
 
 export default ({ children }) => {
     return (
-        <Grid>
+        <div>
             <Header />
             { children }
-        </Grid>
+        </div>
     );
 }
