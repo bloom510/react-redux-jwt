637a31307cf53c81d9cfc477ace3214765f8d4c7
bloom510 <bloomjazzer@gmail.com>
2018-09-10 23:31:47 -0700

    Adds missing module to router

--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 const passportService = require('./services/passport');
 const Authentication = require('./controllers/authentication');
 const passport = require('passport');
+const path = require('path');
 
 const requireAuth = passport.authenticate('jwt', { session: false })
 const requireSignin = passport.authenticate('local', { session: false })
