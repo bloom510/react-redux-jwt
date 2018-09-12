68c4b10c5b7621e6b5e57bd3b9f00fe4a5b1097d
bloom510 <bloomjazzer@gmail.com>
2018-09-12 10:47:40 -0700

    .

--- server/index.js
+++ server/index.js
@@ -23,15 +23,17 @@ class Server {
         );
     }
     initMiddleware() {
-        app.use(cors());
-        app.use(bodyParser.json({ type: '*/*'}));
-        app.use(express.static(path.resolve(__dirname, '..', '..', 'public')))
-        router(app);
 
         app.get('*', function (request, response){
             response.sendFile(path.resolve(__dirname, '../client/public', 'index.html'))
         })
         
+        
+        app.use(cors());
+        app.use(bodyParser.json({ type: '*/*'}));
+        app.use(express.static(path.resolve(__dirname, '..', '..', 'public')))
+        router(app);
+
         if (process.env.NODE_ENV === 'production') 
             app.use(express.static('client/build'));
         if (process.env.NODE_ENV === 'test') 
