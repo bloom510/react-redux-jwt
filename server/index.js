728e91339c759d80ad3036e1b1071784d3b5e4bd
bloom510 <bloomjazzer@gmail.com>
2018-09-12 09:59:32 -0700

    scoping of wildcard routing

--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ class Server {
         app.use(bodyParser.json({ type: '*/*'}));
         app.use(express.static(path.resolve(__dirname, '..', '..', 'public')))
         router(app);
+
+        app.get('*', function (request, response){
+            response.sendFile(path.resolve(__dirname, '../client/public', 'index.html'))
+        })
         
         if (process.env.NODE_ENV === 'production') 
             app.use(express.static('client/build'));
